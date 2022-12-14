d249a12971c5fd22ac8cb58d80c7b9066afbb41f
SammySemicolon <69329424+SammySemicolon@users.noreply.github.com>
2022-12-14 05:30:54 +0100

    Initial commit

---
+++ src/main/java/com/sammy/le_fishe_au_chocolat/LeFisheAuChocolat.java
@@ -0,0 +1,43 @@
+package com.sammy.le_fishe_au_chocolat;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+@Mod(LeFisheAuChocolat.MODID)
+public class LeFisheAuChocolat {
+
+	public static final String MODID = "le_fishe_au_chocolat";
+
+	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
+	public static final DeferredRegister<SoundEvent> SOUNDS = DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, MODID);
+
+	public static final RegistryObject<SoundEvent> CHOIR_OF_LE_FISHE = SOUNDS.register("coeur_du_fishe", () -> new SoundEvent(new ResourceLocation("le_fishe_au_chocolat:coeur_du_fishe")));
+
+	public static final RegistryObject<Item> LE_FISHE_AU_CHOCOLAT = ITEMS.register("le_fishe_au_chocolat", () ->
+			new LeFisheItem(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).food(new FoodProperties.Builder().nutrition(8).saturationMod(0.3F).build())));
+	public static final RegistryObject<Item> LE_FISHE_AU_CHOCOLAT_COOKED = ITEMS.register("le_fishe_au_chocolat_grille", () ->
+			new LeFisheItem(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).food(new FoodProperties.Builder().nutrition(10).saturationMod(0.4F).build())));
+
+	public static final RegistryObject<Item> LE_SAUMON_AU_CHOCOLAT = ITEMS.register("le_saumon_au_chocolat", () ->
+			new LeFisheItem(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).food(new FoodProperties.Builder().nutrition(8).saturationMod(0.3F).build())));
+	public static final RegistryObject<Item> LE_SAUMON_AU_CHOCOLAT_COOKED = ITEMS.register("le_saumon_au_chocolat_grille", () ->
+			new LeFisheItem(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).food(new FoodProperties.Builder().nutrition(10).saturationMod(0.4F).build())));
+
+	public static final RegistryObject<Item> LE_FISHE_AU_CHOCOLAT_EXOTIC = ITEMS.register("le_fishe_exotique_au_chocolat", () ->
+			new LeFisheItem(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).food(new FoodProperties.Builder().nutrition(10).saturationMod(0.4F).build())));
+
+	public LeFisheAuChocolat() {
+		IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
+		ITEMS.register(modBus);
+		SOUNDS.register(modBus);
+	}
+}
---
+++ src/main/java/com/sammy/le_fishe_au_chocolat/LeFisheItem.java
@@ -0,0 +1,29 @@
+package com.sammy.le_fishe_au_chocolat;
+
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.RecordItem;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+import java.util.function.Supplier;
+
+public class LeFisheItem extends RecordItem {
+    public LeFisheItem(Properties builder) {
+        super(7, LeFisheAuChocolat.CHOIR_OF_LE_FISHE, builder, 620);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltip, TooltipFlag pFlag) {
+
+    }
+
+    @Override
+    public MutableComponent getDisplayName() {
+        return Component.translatable("le_fishe_au_chocolat.subtitle.coeur_du_fishe");
+    }
+}
