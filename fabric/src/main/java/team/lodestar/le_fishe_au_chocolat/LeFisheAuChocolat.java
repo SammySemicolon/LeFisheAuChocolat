99fcc45dde5230ca827c083e21de61d004effa00
Biverom <biverom@gmail.com>
2025-04-01 17:38:49 +0200

    1.21.5 fixessssss and modmenu

--- fabric/src/main/java/team/lodestar/le_fishe_au_chocolat/LeFisheAuChocolat.java
+++ fabric/src/main/java/team/lodestar/le_fishe_au_chocolat/LeFisheAuChocolat.java
@@ -1,6 +1,7 @@
 package team.lodestar.le_fishe_au_chocolat;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItem;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
@@ -11,6 +12,8 @@ import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.*;
 
+import java.util.function.Function;
+
 
 public class LeFisheAuChocolat implements ModInitializer {
 
@@ -18,32 +21,37 @@ public class LeFisheAuChocolat implements ModInitializer {
     public static final ResourceKey<JukeboxSong> CHOIR_OF_LE_FISHE_KEY =
             ResourceKey.create(Registries.JUKEBOX_SONG, ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "coeur_du_fishe"));
 
-    public static final LeFisheItem LE_FISHE_AU_CHOCOLAT =
-            new LeFisheItem(new Item.Properties().food(new FoodProperties.Builder().nutrition(8).saturationModifier(0.3F).build()).jukeboxPlayable(CHOIR_OF_LE_FISHE_KEY));
-    public static final LeFisheItem LE_FISHE_AU_CHOCOLAT_COOKED =
-            new LeFisheItem(new Item.Properties().food(new FoodProperties.Builder().nutrition(10).saturationModifier(0.4F).build()).jukeboxPlayable(CHOIR_OF_LE_FISHE_KEY));
-    public static final LeFisheItem LE_SAUMON_AU_CHOCOLAT =
-            new LeFisheItem(new Item.Properties().food(new FoodProperties.Builder().nutrition(8).saturationModifier(0.3F).build()).jukeboxPlayable(CHOIR_OF_LE_FISHE_KEY));
-    public static final LeFisheItem LE_SAUMON_AU_CHOCOLAT_COOKED =
-            new LeFisheItem(new Item.Properties().food(new FoodProperties.Builder().nutrition(10).saturationModifier(0.4F).build()).jukeboxPlayable(CHOIR_OF_LE_FISHE_KEY));
-    public static final LeFisheItem LE_FISHE_AU_CHOCOLAT_EXOTIC =
-            new LeFisheItem(new Item.Properties().food(new FoodProperties.Builder().nutrition(10).saturationModifier(0.4F).build()).jukeboxPlayable(CHOIR_OF_LE_FISHE_KEY));
+    public static final Item LE_FISHE_AU_CHOCOLAT =
+            registerItem("le_fishe_au_chocolat", Item::new, new Item.Properties().food(new FoodProperties.Builder().nutrition(8).saturationModifier(0.3F).build()).jukeboxPlayable(CHOIR_OF_LE_FISHE_KEY));
+    public static final Item LE_FISHE_AU_CHOCOLAT_COOKED =
+            registerItem("le_fishe_au_chocolat_grille", Item::new, new Item.Properties().food(new FoodProperties.Builder().nutrition(10).saturationModifier(0.4F).build()).jukeboxPlayable(CHOIR_OF_LE_FISHE_KEY));
+    public static final Item LE_SAUMON_AU_CHOCOLAT =
+            registerItem("le_saumon_au_chocolat", Item::new, new Item.Properties().food(new FoodProperties.Builder().nutrition(8).saturationModifier(0.3F).build()).jukeboxPlayable(CHOIR_OF_LE_FISHE_KEY));
+    public static final Item LE_SAUMON_AU_CHOCOLAT_COOKED =
+            registerItem("le_saumon_au_chocolat_grille", Item::new, new Item.Properties().food(new FoodProperties.Builder().nutrition(10).saturationModifier(0.4F).build()).jukeboxPlayable(CHOIR_OF_LE_FISHE_KEY));
+    public static final Item LE_FISHE_AU_CHOCOLAT_EXOTIC =
+            registerItem("le_fishe_exotique_au_chocolat", Item::new, new Item.Properties().food(new FoodProperties.Builder().nutrition(10).saturationModifier(0.4F).build()).jukeboxPlayable(CHOIR_OF_LE_FISHE_KEY));
 
     private static SoundEvent registerSound(String id) {
         ResourceLocation identifier = ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, id);
         return Registry.register(BuiltInRegistries.SOUND_EVENT, identifier, SoundEvent.createVariableRangeEvent(identifier));
     }
 
+    public static Item registerItem(String name, Function<Item.Properties, Item> itemFactory, Item.Properties settings) {
+        ResourceKey<Item> itemKey = ResourceKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, name));
+
+        settings = settings.setId(itemKey);
+        Item item = itemFactory.apply(settings);
+
+        Registry.register(BuiltInRegistries.ITEM, itemKey, item);
+
+        return item;
+    }
+
     @Override
     public void onInitialize() {
 
         CommonClass.init();
-
-        Registry.register(BuiltInRegistries.ITEM, ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "le_fishe_au_chocolat"), LE_FISHE_AU_CHOCOLAT);
-        Registry.register(BuiltInRegistries.ITEM, ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "le_fishe_au_chocolat_grille"), LE_FISHE_AU_CHOCOLAT_COOKED);
-        Registry.register(BuiltInRegistries.ITEM, ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "le_saumon_au_chocolat"), LE_SAUMON_AU_CHOCOLAT);
-        Registry.register(BuiltInRegistries.ITEM, ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "le_saumon_au_chocolat_grille"), LE_SAUMON_AU_CHOCOLAT_COOKED);
-        Registry.register(BuiltInRegistries.ITEM, ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "le_fishe_exotique_au_chocolat"), LE_FISHE_AU_CHOCOLAT_EXOTIC);
 
         ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FOOD_AND_DRINKS)
                 .register((creativeTab) -> {
